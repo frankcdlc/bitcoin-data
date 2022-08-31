98ac57f3e284758eecc6899164bb61e7447f1fdf
Frank Cano <frank.cano@utec.edu.pe>
2022-08-30 23:48:06 -0500

    add: validates for block model

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,2 +1,6 @@
 class Block < ApplicationRecord
+  validates :hash_block, presence: true
+  validates :hash_block, uniqueness: true
+  validates :hash_block, format: { with: /\A[0-9a-zA-Z]+\z/,
+                                   message: "only allows letters and numbers" }
 end
